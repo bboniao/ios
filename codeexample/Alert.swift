5394e2f6df3bd30fb6e1c64c4abe772d283e8a09
bboniao <javafanscn@gmail.com>
2015-02-21 21:39:04 +0800

    整理

---
+++ codeexample/Alert.swift
@@ -0,0 +1,11 @@
+import UIKit
+class Alert {
+	@IBAction func showAlert() {
+        //preferredStyle: UIAlertControllerStyle -- ActionSheet/Alert
+        let alert = UIAlertController(title: "Title Test", message: "Message Test", preferredStyle: .ActionSheet)
+        //style: UIAlertActionStyle -- Default/Cancel/Destructive
+        let action = UIAlertAction(title: "Action Test", style: .Destructive, handler: nil)
+        alert.addAction(action);
+        presentViewController(alert, animated: true, completion: nil)
+    }
+}
