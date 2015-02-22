b5c216ddc5a4ae3e50885ab0b42e2ca6a88242ef
bboniao <bboniao@gmail.com>
2015-02-22 15:17:07 +0800

    UIWebViewe的使用

---
+++ codeexample/UIWebView.swift
@@ -0,0 +1,44 @@
+//
+//  AbountViewController.swift
+//  Test
+//
+//  Created by gaara on 2/22/15.
+//  Copyright (c) 2015 gaara. All rights reserved.
+//
+
+import UIKit
+
+class AbountViewController: UIViewController {
+    
+    @IBOutlet weak var webvew: UIWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let htmlFile = NSBundle.mainBundle().pathForResource("BullsEye", ofType: "html") {
+            let htmlData = NSData(contentsOfFile: htmlFile)
+            let baseURL = NSURL.fileURLWithPath(NSBundle.mainBundle().bundlePath)
+            webvew.loadData(htmlData, MIMEType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+        }
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func close() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
